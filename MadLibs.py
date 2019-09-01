bd32c0eb9dd8d92999bbbdc9ff946f591f91d301
trevormiller35 <51308372+trevormiller35@users.noreply.github.com>
2019-09-01 09:49:19 -0500

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,15 @@
+
+def main():
+    print("Let's play some Madlibs.")
+    print("Enter an adjective: ")
+    adj = input()
+    print("Enter a noun: ")
+    nou = input()
+    print("Enter a verb: ")
+    verb = input()
+    print("Enter another noun: ")
+    noun = input()
+
+    print("The", adj, nou, "should never", verb, "the", noun)
+
+main()
